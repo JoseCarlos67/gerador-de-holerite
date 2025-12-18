b1a4755c858e29516c68bac1ae2731235de81332
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-12-17 21:02:29 -0300

    refactor: atributo de lista de folhas de ponto para a classe Employee

--- src/main/java/model/entities/Employee.java
+++ src/main/java/model/entities/Employee.java
@@ -1,16 +1,20 @@
 package model.entities;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Employee {
   private String fullName;
   private String cpf;
   private BigDecimal grossSalary;
+  private List<ClockRecord> clockRecordList;
 
   public Employee(String fullName, String cpf, BigDecimal grossSalary) {
     this.fullName = fullName;
     this.cpf = cpf;
     this.grossSalary = grossSalary;
+    clockRecordList = new ArrayList<>();
   }
 
   public String getFullName() {
@@ -27,5 +31,18 @@ public class Employee {
 
   public void setGrossSalary(BigDecimal value) {
     grossSalary = value;
+  }
+
+  public void addClockRecord(ClockRecord record) {
+    clockRecordList.add(record);
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+            "fullName='" + fullName + '\'' +
+            ", cpf='" + cpf + '\'' +
+            ", grossSalary=" + grossSalary +
+            '}';
   }
 }
